a5e43dc076d3bee213aa132a419141d7372b5f21
wangjie <770499571@qq.com>
2020-07-11 10:47:40 +0800

    表单验证

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-import elementUI from 'element-ui'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import router from '@/router/index'
-Vue.use(elementUI)
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App),
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,40 +4,165 @@
             <div class="header">
                 <img class="left-logo" src="@/assets/img/login_logo.png" alt="">
                 <span class="t1">黑马面面</span>
-                <div class="line">|</div>
+                <div class="line"></div>
                 <span class="t2">用户登录</span>
             </div>
+            <el-form class="form" :model="form" :rules="rules" ref="form">
+                <el-form-item prop="phone">
+                    <el-input v-model="form.phone" placeholder="请输入手机号" prefix-icon="el-icon-user">
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="form.password" placeholder="请输入密码" prefix-icon="el-icon-lock"
+                        :show-password="true">
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="code">
+                    <el-row>
+                        <el-col :span="16">
+                            <el-input placeholder="请输入验证码" v-model="form.code" prefix-icon="el-icon-key">
+                            </el-input>
+                        </el-col>
+                        <el-col :span="8">
+                            <img class="code-img" src="@/assets/img/code.jpg" alt="">
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+                <el-form-item prop="isPass">
+                    <el-checkbox v-model="form.isPass">
+                        我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link type="primary">隐私条款</el-link>
+                    </el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button class="btn" type="primary" @click="submit">登录</el-button>
+                    <br />
+                    <el-button class="btn" type="primary">注册</el-button>
+                </el-form-item>
+            </el-form>
         </div>
         <img src="@/assets/img/login_right.png" alt="">
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            form: {
+                phone: "",
+                password: "",
+                code: "",
+                isPass: ""
+            },
+            rules: {
+                phone: [
+                    {
+                        required: true,
+                        message: "请输入手机号",
+                        trigger: "blur"
+                    },
+                    {
+                        min: 11,
+                        max: 11,
+                        message: "位数不对",
+                        trigger: "blur"
+                    }
+                ],
+                password: [
+                    {
+                        required: true,
+                        message: "请输入密码",
+                        trigger: "change"
+                    },
+                    { min: 6, max: 12, message: "长度不行", trigger: "change" }
+                ],
+                code: [
+                    {
+                        required: true,
+                        message: "请输入验证码",
+                        trigger: "change"
+                    },
+                    { min: 4, max: 4, message: "瞎鸡儿输入" }
+                ],
+                isPass: [
+                    { required: true, message: "钩你丫的", trigger: "change" }
+                ]
+            }
+        };
+    },
+    methods: {
+        submit() {
+            this.$refs.form.validate(result => {
+                if (result) {
+                    this.$message({
+                        message: "6666666",
+                        type: "success"
+                    });
+                } else {
+                    this.$message({
+                        message: "菜鸡",
+                        type: "error"
+                    });
+                }
+            });
+        }
+    }
+};
 </script>
 
 <style lang="less">
 .login {
-    background: linear-gradient(to right, #1494f1, #01c6fa);
-    height: 100%;
     display: flex;
-    align-items: center;
     justify-content: space-around; //!!!!!!!!!!
+    align-items: center;
+    height: 100%;
+    background: linear-gradient(to right, #1494f1, #01c6fa);
     .left {
         width: 478px;
         height: 550px;
         background-color: #f5f5f5;
         padding: 48px;
-        display: flex;
-        align-items: center; //!!!!!!!!!!
-    }
-    .left-logo {
-        width: 22px;
-        height: 17px;
-        margin-right: 20px;
-    }
-    .ti {
-        font-size: 24px;
+        .header {
+            display: flex;
+            align-items: center;
+        }
+        .left-logo {
+            width: 22px;
+            height: 17px;
+            margin-right: 20px;
+        }
+        .t1 {
+            text-align: left;
+            font-size: 22px;
+            font-family: SourceHanSansCN Regular,
+                SourceHanSansCN Regular-Regular;
+            font-weight: 400;
+            color: #0c0c0c;
+        }
+        .line {
+            width: 1px;
+            height: 28px;
+            background-color: #c7c7c7;
+            margin: 0 15px;
+        }
+        .t2 {
+            font-size: 22px;
+            font-family: PingFangSC Regular, PingFangSC Regular-Regular;
+            font-weight: 400;
+            color: #0c0c0c;
+            text-align: left;
+        }
+        .form {
+            margin-top: 20px;
+            .code-img {
+                width: 100%;
+                height: 40px;
+            }
+            .btn {
+                display: block;
+                width: 100%;
+            }
+        }
     }
 }
 </style>
